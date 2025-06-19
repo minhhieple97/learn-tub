fd63adb96d5b521e90e79e8a4110fbb50b1094e5
minhhieple97 <hieplevuc@gmail.com>
2025-06-20 06:38:58 +0700

    refactor: improve performance by use prefetching from react-query

--- components/shared/react-query/get-query-client.ts
+++ components/shared/react-query/get-query-client.ts
@@ -1,23 +1,26 @@
-import { isServer, QueryClient } from "@tanstack/react-query";
+import { QueryClient, defaultShouldDehydrateQuery, isServer } from '@tanstack/react-query';
 
 function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 1000 * 60,
+        staleTime: 60 * 1000,
+      },
+      dehydrate: {
+        shouldDehydrateQuery: (query) =>
+          defaultShouldDehydrateQuery(query) || query.state.status === 'pending',
       },
     },
   });
 }
+
 let browserQueryClient: QueryClient | undefined = undefined;
 
-export const getQueryClient = () => {
+export function getQueryClient() {
   if (isServer) {
     return makeQueryClient();
   } else {
-    if (!browserQueryClient) {
-      browserQueryClient = makeQueryClient();
-    }
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
     return browserQueryClient;
   }
-};
+}
